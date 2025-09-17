97d412ce68f0fdc614a725082ce50e024ab6caae
Eniola Adio <niwani@wikiwoos-MacBook-Pro-2.local>
2025-09-17 13:46:07 +0100

    resolve errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
