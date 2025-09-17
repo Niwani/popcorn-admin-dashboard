7b485359f8a34978c860b249d3049bb4bd84edb2
Eniola Adio <niwani@wikiwoos-MacBook-Pro-2.local>
2025-09-17 14:20:02 +0100

    edit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ export default function Dashboard() {
 
   const handleLogout = () => {
     localStorage.removeItem('token');
-    navigate("/")
+    navigate("/login")
   }
 
   if (loading) return <div>Loading...</div>;
